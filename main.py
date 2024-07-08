b0e2e3d2906c46a193f366cb43bf9dcc3766b71d
Buugless <mateusz.paluch@protonmail.com>
2024-07-08 20:24:24 +0200

    Commit

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from tkinter import Tk,BOTH, Canvas
 
 def main():
     win = Window(800,600)
+    p1 = Point()
+    p1.x = 50
+    p1.y = 50
+
+    p2 = Point()
+    p2.x = 150
+    p2.y = 50
+
+    p3 = Point()
+    p3.x = 100
+    p3.y = 100
+
+    line1 = Line(p1, p2)
+    line2 = Line(p2, p3)
+
+    win.draw_line(line1, "black")
+    win.draw_line(line2, "red")
     win.wait_for_close()
 
 class Window():
@@ -9,7 +26,7 @@ class Window():
         self.width = width
         self.height = height
         self.__root = Tk()
-        self.__root.title("Maze solver")
+        self.__root.title("Maze  solver")
         self.canvas = Canvas(self.__root, width=self.width, height=self.height)
         self.canvas.pack()
         self.run = False
@@ -25,5 +42,19 @@ class Window():
             self.redraw()
     def close(self):
         self.run = False
+    def draw_line(self,line,fill_color):
+        line.draw(self.canvas,fill_color)
 
+
+class Point():
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+class Line():
+    def __init__(self,point1,point2) :
+        self.point1 = point1
+        self.point2 = point2
+    def draw(self,canvas,fill_color):
+        canvas.create_line(self.point1.x,self.point1.y,self.point2.x,self.point2.y,fill=fill_color,width = 2)
 main()
